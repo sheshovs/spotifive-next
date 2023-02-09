254ba44383b9155ee43cb771196da5090b9b803a
Sergio Vargas <sheshovs@gmail.com>
2023-02-09 01:36:48 -0300

    fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { Box, Button } from '@mui/material'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 export default function Home() {
-  const router = useRouter()
+  // const router = useRouter()
   return (
     <>
       <Head>
